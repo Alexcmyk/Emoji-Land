79d84a17dbeb3e5fba225da313359f1248f64b65
Alexcmyk <alex.kirts@gmail.com>
2021-03-13 12:04:07 -0700

    readme and final commit

--- save_to_file.py
+++ save_to_file.py
@@ -8,7 +8,7 @@ os.makedirs("outputs", exist_ok=True)
 noise_levels = [1, 5, 10, 20, 50, 100, 250, 500]
 
 for nl in noise_levels:
-  output = generate_land(100,100, nl)
+  output = generate_land(200,200, nl)
   filename = os.path.join("outputs", f"noise-levels-{nl}.txt")
 
   with open(filename, "w") as f:
